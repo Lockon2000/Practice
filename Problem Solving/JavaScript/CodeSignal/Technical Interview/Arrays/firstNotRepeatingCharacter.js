94b414cc0cd795cd531b271ad9e2748fe23e8a86
Mohamed Abdelwahab <m.taha2000@outlook.de>
2019-06-20 19:28:27 +0200

    Added a JavaScript Solution to CodeSignal/firstNotRepeatingCharacter

---
+++ Problem Solving/JavaScript/CodeSignal/Technical Interview/Arrays/firstNotRepeatingCharacter.js
@@ -0,0 +1,45 @@
+function firstNotRepeatingCharacter(s) {
+    const Store = Object()
+    const SmallACodePoint = 97;
+
+    for (let i = 0; i < 26; ++i) {
+        Store[String.fromCharCode(SmallACodePoint+i)] = {
+            position: null,
+            repetition: 0,
+        }
+    }
+    
+    for (let index in s) {
+        Store[s[index]].repetition += 1;
+        
+        if (Store[s[index]].position === null) {
+            Store[s[index]].position = Number(index) + 1;
+        }
+    }
+    
+    result = '_';
+    minPos = s.length+1;
+    
+    for (let c in Store) {
+        if (Store[c].repetition === 1) {
+            if (Store[c].position < minPos) {
+                result = c;
+                minPos = Store[c].position;
+            }
+        }
+    }
+
+    return result;
+}
+
+// Much faster, but less understandable
+
+firstNotRepeatingCharacter = s => {
+    r = {}
+    for (e of s)
+        r[e] = r[e] ? r[e]+1 : 1
+    for (e in r)
+        if (r[e] == 1)
+            return e
+    return '_'
+}
