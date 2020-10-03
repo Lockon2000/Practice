7be4298be7fe079e8e91e8deb406080272f92564
Mohamed Abdelwahab <m.taha2000@outlook.de>
2020-10-03 03:37:58 +0200

    Implement a corrected Kadane algorithm for the best trade problem

--- Algorithms/Python/Best Trade/trade.py
+++ Algorithms/Python/Best Trade/trade.py
@@ -44,6 +44,29 @@ def trade_kadane(prices):
 
     return (sell_day, buy_day)
 
+def trade_kadane_correct(prices):
+    minimum_price_day = 0
+    best_sell_day = 0
+    best_buy_day = 0
+    best_profit = 0
+
+    for d in range(1, len(prices)):
+        if prices[d] < prices[minimum_price_day]:
+            minimum_price_day = d
+        else:
+            profit = prices[d] - prices[minimum_price_day]
+            if profit > best_profit:
+                best_sell_day = d
+                best_buy_day = minimum_price_day
+                best_profit = profit
+            
+            # THIS IS THE NEEDED PORTION TO FIX THE ABOVE KADANE ALGORITHM
+            # We need not only hold tab of the current best buy/sell pair,
+            # but also of the minimum price till now to be able to correctly
+            # calculate best profits for futrue days.
+                                            
+    return (best_sell_day, best_buy_day)
+
 
 # Example 1
 prices =  [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
@@ -51,11 +74,20 @@ prices =  [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 print(f"{prices=}, {len(prices)=}")
 print(f"{trade_dp(prices)=}")           # Output "(9, 0)"
 print(f"{trade_kadane(prices)=}")       # Output "(9, 0)"
+print(f"{trade_kadane_correct(prices)=}")       # Output "(9, 0)"
 
 # Example 2
 prices =  [3, 3, 1, 2, 5, 7, 11, 9]
 
 print(f"{prices=}, {len(prices)=}")
-print(f"{trade_dp(prices)=}")           # Output "(6, 2)"
-print(f"{trade_kadane(prices)=}")       # Output "(6, 0)" WRONG!!!
+print(f"{trade_dp(prices)=}")               # Output "(6, 2)"
+print(f"{trade_kadane(prices)=}")           # Output "(6, 0)" WRONG!!!
+print(f"{trade_kadane_correct(prices)=}")   # Output "(6, 2)" Correct again!
 
+# Example 3
+prices =  [3, 4, 40, 3, 1, 2, 5, 7, 11, 9]
+
+print(f"{prices=}, {len(prices)=}")
+print(f"{trade_dp(prices)=}")               # Output "(2, 0)"
+print(f"{trade_kadane(prices)=}")           # Output "(2, 0)"
+print(f"{trade_kadane_correct(prices)=}")   # Output "(2, 0)"
