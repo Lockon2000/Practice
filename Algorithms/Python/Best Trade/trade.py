7b0dc0c3eff589b5b8c8a490cadc454dac99f577
Mohamed Abdelwahab <m.taha2000@outlook.de>
2020-10-04 00:47:05 +0200

    Fix comments for Algorithms/Best Trade

--- Algorithms/Python/Best Trade/trade.py
+++ Algorithms/Python/Best Trade/trade.py
@@ -1,23 +1,24 @@
-# Here are two different implementations of the best trade problem.
+# Here are three different implementations for algorithms that solve the best trade problem.
 # trade_dp is of time complexity O(n) and similar space complexity.
 # trade_kadane is of time complexity O(n) and space complexity O(1).
+# trade_kadane_correct is of time complexity O(n) and space complexity O(1).
 # NOTE: trade_kadane is not correct in this form as found in the book
 # computer science distilled. As shown in the second example, the
 # output of the trade_kadane algorithm is obviously incorrect.
 
-def trade_dp(P):
-    B = [None for _ in range(len(P))]
+def trade_dp(prices):
+    B = [None for _ in range(len(prices))]
     B[0] = 0
     sell_day = 0
     best_profit = 0
 
-    for n in range(1, len(P)):
-        if P[n] < P[B[n-1]]:
+    for n in range(1, len(prices)):
+        if prices[n] < prices[B[n-1]]:
             B[n] = n
         else:
             B[n] = B[n-1]
         
-        profit = P[n] - P[B[n]]
+        profit = prices[n] - prices[B[n]]
         if profit > best_profit:
             sell_day = n
             best_profit = profit
@@ -45,7 +46,7 @@ def trade_kadane(prices):
     return (sell_day, buy_day)
 
 def trade_kadane_correct(prices):
-    minimum_price_day = 0
+    minimum_price_day = 0   # You need to hold tab of the minimum price so far to be able to calculate correct profits
     best_sell_day = 0
     best_buy_day = 0
     best_profit = 0
@@ -59,11 +60,6 @@ def trade_kadane_correct(prices):
                 best_sell_day = d
                 best_buy_day = minimum_price_day
                 best_profit = profit
-            
-            # THIS IS THE NEEDED PORTION TO FIX THE ABOVE KADANE ALGORITHM
-            # We need not only hold tab of the current best buy/sell pair,
-            # but also of the minimum price till now to be able to correctly
-            # calculate best profits for futrue days.
                                             
     return (best_sell_day, best_buy_day)
 
@@ -72,9 +68,9 @@ def trade_kadane_correct(prices):
 prices =  [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 print(f"{prices=}, {len(prices)=}")
-print(f"{trade_dp(prices)=}")           # Output "(9, 0)"
-print(f"{trade_kadane(prices)=}")       # Output "(9, 0)"
-print(f"{trade_kadane_correct(prices)=}")       # Output "(9, 0)"
+print(f"{trade_dp(prices)=}")               # Output "(9, 0)"
+print(f"{trade_kadane(prices)=}")           # Output "(9, 0)"
+print(f"{trade_kadane_correct(prices)=}")   # Output "(9, 0)"
 
 # Example 2
 prices =  [3, 3, 1, 2, 5, 7, 11, 9]
